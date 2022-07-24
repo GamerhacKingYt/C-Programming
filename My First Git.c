ffa553717a8f2abaa6bd712ce4ee73c281b4cf29
rafew <mdtahmidfaysal@gmail.com>
2022-07-24 21:48:34 +0600

    my first code in c

--- My First Git.c
+++ My First Git.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("I'm a graphic designer .\n");
+    printf("my hometown is comilla .\n");
+    printf("my friend is Md. Aziz Rahim .");
+    return 0;
+}
 
