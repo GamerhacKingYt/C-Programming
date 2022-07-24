826aa68e60efc6b941d4318d7ca5c077a95a118d
rafew <mdtahmidfaysal@gmail.com>
2022-07-24 23:10:15 +0600

    my first calculator with c

---
+++ my first calculator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    //here's two number sum start
+    printf("Enter 1st number ");
+    int a,b,sum;
+    scanf("%d", &a);
+    printf("Enter 2nd Number ");
+    scanf("%d", &b);
+    printf("a and b sum %d\n", a+b);
+    printf("a and b seb %d\n", a-b);
+    printf("a and b mut %d\n", a*b);
+    printf("a and b div %d\n", a/b);
+    return 0;
+}
